eb3d23855ae1c9cb8d0624e8a50df9f7570bc1bb
tingkun <tingkun@ruqiju.cn>
2021-02-18 13:04:18 +0800

    new

--- sync_jd.sh
+++ sync_jd.sh
@@ -2,7 +2,8 @@
 
 rsync -avvz \
     --exclude='*/data/*' \
-    env.* *.yml ./crmeb *.sh\
+    --exclude='.git*' \
+    env.* *.yml  *.sh\
     php_crmeb redisdata mysqldata\
     devjd:./docker/mshop/
 rsync -avvz source_zip/*.zip  devjd:./docker/mshop/source_zip/
--- upgrade_crmeb.sh
+++ upgrade_crmeb.sh
@@ -9,7 +9,11 @@ cversion=$1
 zipdir=$curdir/source_zip/
 zipfile=crmeb_merchant-${cversion}.zip
 
+[[ ! -f $zipdir/$zipfile  ]] && echo "$zipdir/$zipfile not exist." && \
+    zipfile=crmeb_merchant_${cversion}.zip
+
 [[ ! -f $zipdir/$zipfile  ]] && echo "$zipdir/$zipfile not exist." && exit
+
 
 unzipdir=$curdir/source_zip/$cversion.crmeb
 mkdir -p $unzipdir
@@ -24,8 +28,18 @@ cd $cmdir && unzip compiled74.zip
 cp -rf $cmdir/basic $unzipdir/crmewb/
 cp -rf $cmdir/crmeb.php $unzipdir/config/
 
-bakupdir=$curdir/bakupdir/$(date +%Y%m%d%H%I%S).crmeb;
-mkdir -p $bakupdir
-rsync -avvz  $curdir/crmeb/* $bakupdir/
+#配置文件覆盖
+cp -f  $curdir/.env  $unzipdir/
+
+#备份
+if [  $2 != 'nob' ] ; then
+    bakupdir=$curdir/bakupdir/$(date +%Y%m%d%H%I%S).crmeb;
+    mkdir -p $bakupdir
+    rsync -avvz  $curdir/crmeb/* $bakupdir/
+fi
+
 mkdir -p $curdir/crmeb
+
 rsync -avv $unzipdir/*  $curdir/crmeb/
+cp $unzipdir/.version $curdir/crmeb/
+
